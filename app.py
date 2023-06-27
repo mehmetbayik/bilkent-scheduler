520d2533743e2c5e7c8fec182152e4a9c5e31edb
mehmetb35 <mehmetb35@hotmail.com>
2023-06-28 02:28:16 +0300

    feature added: switching tables

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from scheduler_class import CourseScheduleExtractor
+import pandas as pd
 
 app = Flask(__name__, static_folder='static')
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def index():
@@ -33,21 +35,45 @@ def schedule():
     # Generate the schedules and retrieve necessary data
     schedules = scheduler.get_schedules()
     num_schedules = len(schedules)
-    '''
-    # Get the current schedule index from the form submission
-    current_schedule = int(request.form.get('current_schedule', 0))
+
+    # Convert the DataFrame to a list of dictionaries
+    schedules_json = []
+    for schedule in schedules:
+        schedules_json.append(schedule.to_dict(orient='records'))
+
+    # Store the schedules and num_schedules in the session
+    session['schedules'] = schedules_json
+    session['num_schedules'] = num_schedules
+
+    # Redirect to the schedules route
+    return redirect(url_for('schedules'))
+
+@app.route('/schedules', methods=['GET', 'POST'])
+def schedules():
+    # Retrieve the schedules and num_schedules from the session
+    schedules_json = session.get('schedules', [])
+    num_schedules = session.get('num_schedules', 0)
+
+    # Convert the schedules back to DataFrame
+    schedules = []
+    for schedule in schedules_json:
+        schedules.append(pd.DataFrame(schedule))
+
+    # Get the current schedule index from the session
+    current_schedule = session.get('current_schedule', 0)
 
     # Adjust the current schedule index based on the button clicked
-    if request.form['submit'] == 'prev':
-        current_schedule -= 1
-    elif request.form['submit'] == 'next':
-        current_schedule += 1
-    
-    # Handle boundary cases where the index goes beyond the schedule list length
-    current_schedule = max(0, min(current_schedule, num_schedules - 1))
-    '''
-    # Pass the data to the template for rendering
-    return render_template('schedule.html', schedules=schedules, num_schedules=num_schedules)
+    if request.method == 'POST':
+        if request.form['submit'] == 'prev':
+            current_schedule = max(0, current_schedule - 1)
+        elif request.form['submit'] == 'next':
+            current_schedule = min(current_schedule + 1, num_schedules - 1)
+
+    # Update the current schedule index in the session
+    session['current_schedule'] = current_schedule
+
+    # Render the schedule template with the current schedule
+    return render_template('schedule.html', schedules=schedules, num_schedules=num_schedules, current_schedule=current_schedule)
 
 
 if __name__ == '__main__':
